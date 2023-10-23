2a313ea15ce6a2a775d880f3b463c0b2e21aac61
LuQingTong <147984594+LuQingTong@users.noreply.github.com>
2023-10-23 17:30:58 +0800

    Delete 02 函数与程序 directory

--- 02 函数与程序/14.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |14|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
-(define (string-last str)
-  (if (> (string-length str) 0)
-      (string-ith str (-(string-length str) 1))
-      "your string has nothing"))
-
-;string-ith 中i从0计数，最后一位子字符串的i值是字符串长度-1
--- 02 函数与程序/30(？).rkt
+++
@@ -1,8 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |30|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-;常量定义
-(define base-ticket-price 5)
-(define price-change 0.1)
-(define attendees-change 15)
-(define sensitivity (* (/ (- ticket-price base-ticket-price) price-change) attendees-change))
